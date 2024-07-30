bec51e3638d4237044f9e0c737a19b4dca89b56f
hyojin KIM <yellomouse05@gmail.com>
2024-07-30 15:32:32 +0900

    Style: main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class Main {
-    public static void main(String[] args) {
-
-    }
+  public static void main(String[] args) {
+    System.out.println("making a movie ticket");
+  }
 }
