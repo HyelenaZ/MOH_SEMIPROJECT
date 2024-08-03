d74d9a000124008dcb88c39156fe1493270c3ac1
hyojin KIM <yellomouse05@gmail.com>
2024-08-03 22:52:02 +0900

    Resolved merge conflicts

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,9 +6,15 @@ import users.UsersService;
 import users.UsersServiceImpl;
 
 public class Main {
+<<<<<<< HEAD
     public static void main(String[] args) {
         UsersService userService = new UsersServiceImpl();
         MainServiceImpl mainService = new MainServiceImpl(userService);
         mainService.execute();
     }
+=======
+  public static void main(String[] args) {
+    System.out.println("Booking a movie ticket");
+  }
+>>>>>>> e4012e58a5aab744775e2af1ecd76f0eb54cab3e
 }
